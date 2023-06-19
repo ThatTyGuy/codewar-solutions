eb2758bf5b1b626b890cd41220c22a75d43e73e8
Tyler Sunderland <134873559+ThatTyGuy@users.noreply.github.com>
2023-06-19 15:46:36 -0400

    Create up-and-down-the-string-grows.js
    First project

---
+++ up-and-down-the-string-grows.js
@@ -0,0 +1,7 @@
+/* Many programming languages provide the functionality of converting a string to uppercase or lowercase. Typically, these methods won't change the size of the string. However, in some special cases, the length of the transformed string may be longer than the original.
+
+Since the following string contains special characters. When STRANGE_STRING.toUpperCase().toLowerCase().length; is executed. More than 1 character is returned.
+
+*/
+
+const STRANGE_STRING = "İ"
